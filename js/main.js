e9afaf6d30af9e234aa84f80a8a6465bf3ff9aa8
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-04-26 19:18:37 +0600

    added 90 persend

--- js/main.js
+++ js/main.js
@@ -9,3 +9,22 @@ toggleButton.addEventListener("click", () => {
 closeButton.addEventListener("click", () => {
   sidebar.classList.remove("active");
 });
+
+// COurse Detils Accordian styleing
+
+document.addEventListener("DOMContentLoaded", function () {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+});
