5e0b411d3f4325199f1780772b5757c8503ff592
Md Fahim Muntashir <101015239+Fahim-Muntashir@users.noreply.github.com>
2024-10-21 10:52:20 +0600

    Responsive Layout and the Course Details

--- js/main.js
+++ js/main.js
@@ -1,23 +1,19 @@
-const toggleButton = document.getElementById("header-toggle");
-const closeButton = document.getElementById("header-close");
-const sidebar = document.getElementById("sidebar");
-
-toggleButton.addEventListener("click", () => {
-  sidebar.classList.toggle("active");
-});
-
-closeButton.addEventListener("click", () => {
-  sidebar.classList.remove("active");
-});
-
-// COurse Detils Accordian styleing
-
 document.addEventListener("DOMContentLoaded", function () {
   var acc = document.getElementsByClassName("accordion");
   var i;
 
   for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function () {
+      // Close all other panels
+      for (var j = 0; j < acc.length; j++) {
+        if (acc[j] !== this) {
+          acc[j].classList.remove("active");
+          var otherPanel = acc[j].nextElementSibling;
+          otherPanel.style.maxHeight = null;
+        }
+      }
+
+      // Toggle the clicked panel
       this.classList.toggle("active");
       var panel = this.nextElementSibling;
       if (panel.style.maxHeight) {
